6237aeda72018fca04c1812a7d05c4718c16d22e
Mellowstar <nicolas1.ramanantsoa@epitech.eu>
2025-12-13 18:14:49 +0100

    feat: logger + tracer

---
+++ src/core/utils.ts
@@ -0,0 +1,12 @@
+import { Logger } from "@aws-lambda-powertools/logger";
+import { Tracer } from "@aws-lambda-powertools/tracer";
+import { z } from "zod";
+
+export const env = z
+  .object({
+    SERVICE: z.string(),
+  })
+  .parse(process.env);
+
+export const logger = new Logger({ serviceName: env.SERVICE });
+export const tracer = new Tracer({ serviceName: env.SERVICE });
