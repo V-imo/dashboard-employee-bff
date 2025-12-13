b7d54dba945fde05a76dd30e0948a5c119e4a9c6
Mellowstar <nicolas1.ramanantsoa@epitech.eu>
2025-12-13 17:01:08 +0100

    added: necessary projen deps

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,24 @@
-import { Projalf } from "projalf"
+import { Projalf } from "projalf";
 const project = new Projalf({
   cdkVersion: "2.1.0",
   defaultReleaseBranch: "main",
   devDeps: ["projalf"],
+  deps: [
+    "@aws-lambda-powertools/logger",
+    "@aws-lambda-powertools/tracer",
+
+    "zod",
+
+    "hono",
+    "@hono/zod-openapi",
+    "@hono/swagger-ui",
+
+    "@aws-sdk/client-cognito-identity-provider",
+  ],
   name: "dashboard-employee-bff",
   projenrcTs: true,
 
-  // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
-})
-project.synth()
+});
+project.synth();
