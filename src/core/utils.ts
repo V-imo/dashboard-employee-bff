96ee8d5a3e35fc43ecf62932ad5932a4bee1b108
Mellowstar <nicolas1.ramanantsoa@epitech.eu>
2026-03-17 14:11:53 +0100

    deleted unused  env var

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -5,8 +5,6 @@ import { z } from "zod";
 export const env = z
   .object({
     SERVICE: z.string(),
-    USER_POOL_ID: z.string().default(""),
-    COGNITO_CLIENT_ID: z.string().default(""),
     TABLE_NAME: z.string().default(""),
     EVENT_BUS_NAME: z.string().default(""),
     STAGE: z.string().default(""),
